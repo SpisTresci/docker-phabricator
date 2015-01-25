8fca536a078066f2dc294afbb68142900aaee53d
Krzysztof Szumny <krzysztof.szumny@spistresci.pl>
2015-01-25 13:46:06 +0100

    T688: Automatyczne tworzenie backupów na serwerze dev.spistresci.pl nie działa

--- make_backup.sh
+++ make_backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 fig stop
-archive_filename=phabricator-backup.tar.bz2
+archive_filename=phabricator-backup.tar
 
-docker run -i -t --volumes-from dockerphabricator_data_1 -v $(pwd):/backup/ ubuntu tar jcvf /backup/${archive_filename} /var/lib/mysql /var/repo /opt/phabricator/conf /etc/ssl/spistresci
+docker run --volumes-from dockerphabricator_data_1 -v $(pwd):/backup/ ubuntu tar cvf /backup/${archive_filename} /var/lib/mysql /opt/phabricator/conf /etc/ssl/spistresci
 
 fig up -d
 
--- restore_backup.sh
+++ restore_backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 fig stop
-archive_filename=phabricator-backup.tar.bz2
+archive_filename=phabricator-backup.tar
 
 bakthat restore $archive_filename
-docker run --volumes-from dockerphabricator_data_1 -v $(pwd):/backup ubuntu tar -vxjf /backup/${archive_filename}
+docker run --volumes-from dockerphabricator_data_1 -v $(pwd):/backup ubuntu tar -vxf /backup/${archive_filename}
 
 fig up -d
 
